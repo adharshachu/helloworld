3fb3968b50dd88827c0516902245deb8fa45e36e
Adharsh GS <71245685+adharshachu@users.noreply.github.com>
2021-06-24 12:30:10 +0530

    First commit

---
+++ Hello world/src/helloworld/Basics.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sentence = ("Hello World");
+		System.out.println(sentence);
+
+	}
+
+}
